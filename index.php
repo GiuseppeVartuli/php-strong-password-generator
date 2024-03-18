36ed5d9eaed8c6b5cec1257e5e2fecd72b9a08d3
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-03-18 21:56:28 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="GET">
+        <label for="password_length">Lunghezza password:</label>
+        <input type="number">
+        <button type="submit">Genera Password</button>
+    </form>
+</body>
+</html>

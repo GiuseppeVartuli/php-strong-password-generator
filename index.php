f536b0df676da09faf72c04d9bc41b3a7dfee7c0
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-04-02 11:46:46 +0200

    milestone 2

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
 
 <?php
 
-// Funzione per generare una password casuale
-function RandomPassword($length) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+';
-    $password = '';
-
-    // Genera la password casuale
-    for ($i = 0; $i < $length; $i++) {
-        $password .= $characters[rand(0, strlen($characters) - 1)];
-    }
-
-    return $password;
-}
-
-// Se la lunghezza della password è stata inviata tramite GET
-if (isset($_GET['password_length'])) {
-    $password_length = $_GET['password_length'];
-
-    // Genera e mostra la password solo se la lunghezza è valida
-    if ($password_length > 5) {
-        $random_password = RandomPassword($password_length);
-        echo "<h2>Password generata:</h2>";
-        echo "<p>$random_password</p>";
-    } else {
-        echo "<p style='color: red;'>Selezionare la lunghezza della password di almeno 6 caratteri</p>";
-    }
-}
+require_once __DIR__ . '/functions.php'
 ?>
 
 <!DOCTYPE html>
